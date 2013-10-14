4eb0dad78bf0d4d641ca0ac19abc633780338d1a
RobinTang <sintrb@gmail.com>
2013-10-15 01:21:25 +0800

    登陆申明

--- BaiduUser.py
+++ BaiduUser.py
@@ -7,19 +7,29 @@ Created on 2013-10-14
 
 from HttpHolder import HttpHolder
 
+
 class BaseBaiduUser():
 	'''
 	基础百度用户
 	'''
-	def __init__(self, cookies=None):
+	def __init__(self, username=None, password=None, cookies=None):
 		'''
 		创建一个BaseBaiduUser实例，可以指定拜user中Http请求的最初Cookie
 		'''
 		self.http = HttpHolder()
+		self.username = username
+		self.password = password
 		if cookies:
 			self.http.set_cookiesdict(cookies)
 
-
+	def login(self):
+		'''
+		用户登陆
+		'''
+		return False
+		
 if __name__ == '__main__':
-	pass
+	bdu = BaseBaiduUser('username', 'password')
+	print bdu.login()
+	
 
